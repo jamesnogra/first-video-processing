95c32580cb5805b443b5595b2de57b82fb9602b0
jamesnogra <love.gerald@yahoo.com>
2017-07-22 21:00:00 +0800

    First push.

---
+++ VideoProcessing/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace VideoProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cameraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setStaticImageFromLiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBackgroundImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startBroadcastingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.thresholdBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.staticImageFromLive = new System.Windows.Forms.PictureBox();
+            this.backgroundImage = new System.Windows.Forms.PictureBox();
+            this.browseBGImageDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.staticImageFromLive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cameraToolStripMenuItem,
+            this.setImagesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(670, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cameraToolStripMenuItem
+            // 
+            this.cameraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.disconnectToolStripMenuItem});
+            this.cameraToolStripMenuItem.Name = "cameraToolStripMenuItem";
+            this.cameraToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.cameraToolStripMenuItem.Text = "Camera";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.connectToolStripMenuItem.Text = "Connect";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.disconnectToolStripMenuItem.Text = "Disconnect";
+            this.disconnectToolStripMenuItem.Click += new System.EventHandler(this.disconnectToolStripMenuItem_Click);
+            // 
+            // setImagesToolStripMenuItem
+            // 
+            this.setImagesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setStaticImageFromLiveToolStripMenuItem,
+            this.openBackgroundImageToolStripMenuItem,
+            this.startBroadcastingToolStripMenuItem});
+            this.setImagesToolStripMenuItem.Name = "setImagesToolStripMenuItem";
+            this.setImagesToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.setImagesToolStripMenuItem.Text = "Set Images";
+            // 
+            // setStaticImageFromLiveToolStripMenuItem
+            // 
+            this.setStaticImageFromLiveToolStripMenuItem.Name = "setStaticImageFromLiveToolStripMenuItem";
+            this.setStaticImageFromLiveToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.setStaticImageFromLiveToolStripMenuItem.Text = "Set Static Image From Live";
+            this.setStaticImageFromLiveToolStripMenuItem.Click += new System.EventHandler(this.setStaticImageFromLiveToolStripMenuItem_Click);
+            // 
+            // openBackgroundImageToolStripMenuItem
+            // 
+            this.openBackgroundImageToolStripMenuItem.Name = "openBackgroundImageToolStripMenuItem";
+            this.openBackgroundImageToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.openBackgroundImageToolStripMenuItem.Text = "Open Background Image";
+            this.openBackgroundImageToolStripMenuItem.Click += new System.EventHandler(this.openBackgroundImageToolStripMenuItem_Click);
+            // 
+            // startBroadcastingToolStripMenuItem
+            // 
+            this.startBroadcastingToolStripMenuItem.Name = "startBroadcastingToolStripMenuItem";
+            this.startBroadcastingToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.startBroadcastingToolStripMenuItem.Text = "Start Broadcasting";
+            this.startBroadcastingToolStripMenuItem.Click += new System.EventHandler(this.startBroadcastingToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(338, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // thresholdBar
+            // 
+            this.thresholdBar.Location = new System.Drawing.Point(12, 289);
+            this.thresholdBar.Maximum = 100;
+            this.thresholdBar.Name = "thresholdBar";
+            this.thresholdBar.Size = new System.Drawing.Size(646, 45);
+            this.thresholdBar.TabIndex = 3;
+            this.thresholdBar.Value = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(309, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Threshold";
+            // 
+            // staticImageFromLive
+            // 
+            this.staticImageFromLive.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.staticImageFromLive.Location = new System.Drawing.Point(12, 327);
+            this.staticImageFromLive.Name = "staticImageFromLive";
+            this.staticImageFromLive.Size = new System.Drawing.Size(320, 240);
+            this.staticImageFromLive.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.staticImageFromLive.TabIndex = 7;
+            this.staticImageFromLive.TabStop = false;
+            // 
+            // backgroundImage
+            // 
+            this.backgroundImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.backgroundImage.Location = new System.Drawing.Point(338, 327);
+            this.backgroundImage.Name = "backgroundImage";
+            this.backgroundImage.Size = new System.Drawing.Size(320, 240);
+            this.backgroundImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.backgroundImage.TabIndex = 8;
+            this.backgroundImage.TabStop = false;
+            // 
+            // browseBGImageDialog
+            // 
+            this.browseBGImageDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 578);
+            this.Controls.Add(this.backgroundImage);
+            this.Controls.Add(this.staticImageFromLive);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.thresholdBar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Video Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.staticImageFromLive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cameraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem setImagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setStaticImageFromLiveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openBackgroundImageToolStripMenuItem;
+        private System.Windows.Forms.TrackBar thresholdBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox staticImageFromLive;
+        private System.Windows.Forms.PictureBox backgroundImage;
+        private System.Windows.Forms.OpenFileDialog browseBGImageDialog;
+        private System.Windows.Forms.ToolStripMenuItem startBroadcastingToolStripMenuItem;
+    }
+}
+
---
+++ VideoProcessing/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebCamLib;
+using ImageProcess2;
+using System.Drawing.Imaging;
+
+namespace VideoProcessing
+{
+    public partial class Form1 : Form
+    {
+        Device[] allDevices;
+        Bitmap source, staticLiveImage, bgImage;
+        byte edgeDetectType;
+        bool startBroadcast = false;
+        int width = 640, height = 480;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            allDevices = DeviceManager.GetAllDevices();
+            allDevices[0].ShowWindow(pictureBox1);
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //try searching tagVideoProcAmpProperty
+            // this code captures snapshots
+            IDataObject data;
+            Image bmap;
+            //int edgeDetectThreshold;
+            allDevices[0].Sendmessage();
+            data = Clipboard.GetDataObject();
+            bmap = (Image)(data.GetData("System.Drawing.Bitmap", true));
+            source = new Bitmap(bmap);
+            //BitmapFilter.Resize(source, width, height, true);
+
+            if (startBroadcast)
+            {
+                this.CustomSubtract(source, staticLiveImage, bgImage, (byte)thresholdBar.Value);
+            }
+
+
+            //grayscale the image for easier image processing
+            /*BitmapFilter.GrayScale(source);
+
+            if (edgeDetectType != 0)
+            {
+                edgeDetectThreshold = thresholdBar.Value;
+                BitmapFilter.EdgeDetectConvolution(source, edgeDetectType, (byte)edgeDetectThreshold);
+            }*/
+
+            pictureBox2.Image = source;
+        }
+
+        private void disconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void sobelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            edgeDetectType = 3;
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            edgeDetectType = 0;
+        }
+
+        private void kirshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            edgeDetectType = 1;
+        }
+
+        private void prewittToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            edgeDetectType = 2;
+        }
+
+        private void startBroadcastingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            startBroadcast = true;
+        }
+
+        private void openBackgroundImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            browseBGImageDialog.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Bitmap newBg = new Bitmap(browseBGImageDialog.FileName);
+            bgImage = new Bitmap(width, height);
+            //BitmapFilter.Resize(bgImage, 640, 480, true);
+            this.CustomResize(ref newBg, ref bgImage, width, height);
+            //MessageBox.Show("Resized to " + bgImage.Width + "x" + bgImage.Height);
+            backgroundImage.Image = bgImage;
+        }
+
+        private void setStaticImageFromLiveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IDataObject data;
+            Image bmap;
+            allDevices[0].Sendmessage();
+            data = Clipboard.GetDataObject();
+            bmap = (Image)(data.GetData("System.Drawing.Bitmap", true));
+            staticLiveImage = new Bitmap(bmap);
+            //BitmapFilter.Resize(staticLiveImage, width, height, false);
+            staticImageFromLive.Image = staticLiveImage;
+            //MessageBox.Show("Resized to " + staticLiveImage.Width + "x" + staticLiveImage.Height);
+        }
+
+        public bool CustomSubtract(Bitmap source, Bitmap staticImg, Bitmap bg, byte threshold)
+        {
+            // GDI+ still lies to us - the return format is BGR, NOT RGB.
+            BitmapData sourceLive = source.LockBits(new Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData staticFromLive = staticImg.LockBits(new Rectangle(0, 0, staticImg.Width, staticImg.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData bgForLive = bg.LockBits(new Rectangle(0, 0, bg.Width, bg.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            //live video feed
+            int strideLiveVideo = sourceLive.Stride;
+            System.IntPtr sourceLiveStartPointer = sourceLive.Scan0;
+
+            //static Img
+            int strideStaticImage = staticFromLive.Stride;
+            System.IntPtr staticImgStartPointer = staticFromLive.Scan0;
+
+            //Background
+            int strideBg = bgForLive.Stride;
+            System.IntPtr bgStartPointer = bgForLive.Scan0;
+
+            unsafe
+            {
+                byte* sourceLiveMovingPointer = (byte*)(void*)sourceLiveStartPointer;
+                byte* staticImgMovingPointer = (byte*)(void*)staticImgStartPointer;
+                byte* bgMovingPointer = (byte*)(void*)bgStartPointer;
+                int liveVideoOffset = strideLiveVideo - source.Width * 3;
+                int staticImageOffset = strideStaticImage - staticImg.Width * 3;
+                int bgOffset = strideBg - bg.Width * 3;
+                int nWidth = source.Width * 3;
+                Color tempLive, tempStatic;
+
+                for (int y = 0; y < source.Height; ++y)
+                {
+                    for (int x = 0; x < source.Width; ++x)
+                    {
+                        
+                        tempLive = Color.FromArgb(sourceLiveMovingPointer[2], sourceLiveMovingPointer[1], sourceLiveMovingPointer[0]);
+                        tempStatic = Color.FromArgb(staticImgMovingPointer[2], staticImgMovingPointer[1], staticImgMovingPointer[0]);
+                        //if (Math.Abs(p[0]-c[0])<threshold && Math.Abs(p[1]-c[1])<threshold && Math.Abs(p[2]-c[2])<threshold)
+                        if (Math.Abs(sourceLiveMovingPointer[0] - staticImgMovingPointer[0]) < threshold &&
+                            Math.Abs(sourceLiveMovingPointer[1] - staticImgMovingPointer[1]) < threshold &&
+                            Math.Abs(sourceLiveMovingPointer[2] - staticImgMovingPointer[2]) < threshold)
+                        //if (Math.Abs(tempLive.GetHue()-tempStatic.GetHue())<threshold && Math.Abs(tempLive.GetSaturation()-tempStatic.GetSaturation())<threshold)
+                        {
+                            sourceLiveMovingPointer[0] = bgMovingPointer[0];
+                            sourceLiveMovingPointer[1] = bgMovingPointer[1];
+                            sourceLiveMovingPointer[2] = bgMovingPointer[2];
+                        }
+                        sourceLiveMovingPointer += 3;
+                        staticImgMovingPointer += 3;
+                        bgMovingPointer += 3;
+                    }
+                    sourceLiveMovingPointer += staticImageOffset;
+                    staticImgMovingPointer += staticImageOffset;
+                    bgMovingPointer += bgOffset;
+                }
+            }
+            source.UnlockBits(sourceLive);
+            bg.UnlockBits(bgForLive);
+            staticImg.UnlockBits(staticFromLive);
+            return true;
+        }
+
+        public void CustomResize(ref Bitmap source, ref Bitmap result, int width, int height)
+        {
+            result = new Bitmap(width, height); //initalize the image
+            Color pixel;
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    pixel = source.GetPixel(x * source.Width / width, y * source.Height / height);
+                    result.SetPixel(x, y, pixel);
+                }
+            }
+        }
+    }
+}
